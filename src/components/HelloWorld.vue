349d6f414cf89135ce9815eacea911a630a688b1
lloosky <karolswitalla@gmail.com>
2023-04-03 23:03:22 +0200

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    TEST {{ test }}
+    <v-btn @click="add">ADD</v-btn>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      test: 1
+    }
+  },
+  methods: {
+    add() {
+      this.test += 1;
+    }
+  }
+})
+</script>
